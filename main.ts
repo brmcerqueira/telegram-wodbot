a8873d0dc22b2b3179955a92dc3e9914066baf80
Bruno <brmcerqueira@gmail.com>
2023-04-10 16:50:32 -0300

    Adjust

--- main.ts
+++ main.ts
@@ -1,40 +1,64 @@
-import { TelegramBot, UpdateType } from "https://deno.land/x/telegram_bot_api@0.4.0/mod.ts"
+import { BotCommand, TelegramBot, UpdateType } from "https://deno.land/x/telegram_bot_api@0.4.0/mod.ts"
 
 const TOKEN = Deno.env.get("WODBOT_TOKEN");
+
 if (!TOKEN) throw new Error("Bot token is not provided");
+
 const bot = new TelegramBot(TOKEN);
 
+const upload: BotCommand = {
+  command: "enviar",
+  description: "Comando para fazer o upload da ficha"
+};
+
+const download: BotCommand = {
+  command: "baixar",
+  description: "Comando para fazer o download da ficha"
+};
+
 await bot.setMyCommands(<any>{
-  commands: [{
-    command: "upload",
-    description: "Comando para fazer o upload da ficha"
-  }, {
-    command: "download",
-    description: "Comando para fazer o download da ficha"
-  }],
+  commands: [upload, download],
   scope: {
     type: "all_group_chats"
   }
 })
 
+const me = await bot.getMe();
+
+console.log(me);
+
 bot.on(UpdateType.Message, async ({ message }) => {
   console.log(message);
-  if (message.text?.startsWith("/download@vampire_the_masquerade_5th_bot")) {
+  const name = message.from?.username || message.from?.first_name;
+  if (message.text?.startsWith(`/${upload.command}@${me.username}`)) {
     await bot.sendMessage({
       chat_id: message.chat.id,
-      text: "Sua ficha foi enviada com sucesso!",
+      text: `@${name} Envie sua ficha em anexo, para que eu possa salvar!`
+    });
+  }
+  else if (message.text?.startsWith(`/${download.command}@${me.username}`)) {
+    await bot.sendMessage({
+      chat_id: message.chat.id,
+      text: `@${name} Sua ficha foi enviada na sua caixa privada!`
+    });
+  }
+  else if (message.document) {
+    console.log(message.document);
+    await bot.sendMessage({
+      chat_id: message.chat.id,
+      text: `@${name} Sua ficha foi enviada com sucesso!`
     });
   }
   else {
     const text = message.text || "I can't hear you";
     await bot.sendMessage({
       chat_id: message.chat.id,
-      text: "Você disse: " + text,
+      text: "Você disse: " + text
     });
   }
 });
 
-
 bot.run({
   polling: true,
+  
 });
